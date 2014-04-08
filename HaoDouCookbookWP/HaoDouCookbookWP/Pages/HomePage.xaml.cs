9efe69dc920fe29bde4247f789a83fa59ef2bf90
elecpiano <elecpiano@gmail.com>
2014-04-08 09:49:55 +0800

    initial upload

---
+++ HaoDouCookbookWP/HaoDouCookbookWP/Pages/HomePage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace HaoDouCookbookWP.Pages
+{
+    public partial class HomePage : PhoneApplicationPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        #region Panorama Selection
+
+        private void Panorama_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            topbar.Flirt();
+            switch (panorama.SelectedIndex)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
